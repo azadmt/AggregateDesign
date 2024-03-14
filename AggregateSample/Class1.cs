f533346b9a5413ff033c64f758454d9e1925a8fd
m.azad <m.azad@xaniis.com>
2024-03-14 11:56:57 +0330

    init

---
+++ AggregateSample/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.Common;
+
+namespace AggregateSample
+{
+    public class Auction
+    {
+        public Auction(Guid id, Guid itemId, Money startingPrice, DateTime endsAt)
+        {
+        }
+
+        public Guid ItemId { get; set; }
+        public DateTime EndsAt { get; set; }
+        public Money StartingPrice { get; set; }
+        public WinningBid WinningBid { get; set; }
+
+        public void PlaceBidFor(Guid biderId, Money bidPrice, DateTime currentTime)
+        {
+            if (bidPrice > WinningBid.Price)
+            {
+                WinningBid = new WinningBid(biderId, bidPrice, currentTime);
+            }
+        }
+    }
+}
+
+public class Money
+{
+    public decimal Value { get; set; }
+
+    public static bool operator <(Money l, Money r)
+    {
+        return l.Value < r.Value;
+    }
+
+    public static bool operator >(Money l, Money r)
+    {
+        return l.Value > r.Value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Value.Equals(((Money)obj).Value);
+    }
+}
+
+public class WinningBid
+{
+    public Guid BiderId { get; private set; }
+
+    public WinningBid(Guid biderId, Money price, DateTime bidTime)
+    {
+        BiderId = biderId;
+        Price = price;
+        BidTime = bidTime;
+    }
+
+    public Money Price { get; private set; }
+    public DateTime BidTime { get; private set; }
+}
---
+++ FoodReservation/Model.cs
@@ -0,0 +1,31 @@
+﻿namespace FoodReservation
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public LinkedList<FoodMaterial> FoodMaterials { get; set; }
+    }
+
+    public class FoodMaterial
+    {
+        public int ProductId { get; set; }
+        public double Measure { get; set; }
+        public string MeasureType { get; set; }
+    }
+
+    public class FoodCalendar
+    {
+        public int FoodId { get; set; }
+        public int Capacity { get; set; }
+    }
+
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public decimal ReservAuantity { get; set; }
+    }
+}
